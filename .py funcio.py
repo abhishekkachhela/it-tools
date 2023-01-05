1cb46ae43c5864266fc493ec2491164f6789c40f
abhishekkachhela <121996639+abhishekkachhela@users.noreply.github.com>
2023-01-05 09:47:16 +0530

    Add files via upload

---
+++ .py funcio.py
@@ -0,0 +1,8 @@
+num = int ("enter a number "))
+parameter =4*num
+print ("the parameter of square is",square)
+
+r=int (input("enter a r")
+       pi=3,14
+       parameter=(2*pu*r)
+       print ("the parameter of square is " circle)
